9b9cee3523ca3542b2a0a526dd088bf0c6da327c
alex <vpodrezov@jeepshop.local>
2024-02-07 11:28:29 +0300

    one

---
+++ tests/UserTableWrapperTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTableWrapperTest extends TestCase
+{
+    // Тесты для методов insert, update, delete, get
+}
---
+++ tests/UsersTableWrapperTest.php
@@ -0,0 +1,8 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UsersTableWrapperTest extends TestCase
+{
+    // Тесты для методов insert, update, delete, get
+}
